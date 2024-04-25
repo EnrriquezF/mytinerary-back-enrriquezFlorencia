16d858561afa3be978025466e7e4003afbebe02a
EnrriquezF <102559480+EnrriquezF@users.noreply.github.com>
2024-04-25 20:30:51 -0300

    test

--- src/app.js
+++ src/app.js
@@ -10,9 +10,7 @@ app.use(express.json())
 app.use(cors())
 app.use(helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self'"],
       imgSrc: ["'self'"]
-      // Add other directives as needed
     }
   }));
 
