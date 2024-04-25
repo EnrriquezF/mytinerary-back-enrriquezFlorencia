553fc800d3d6305a33ca7c78fd653798e6d24b26
EnrriquezF <102559480+EnrriquezF@users.noreply.github.com>
2024-04-25 19:07:14 -0300

    auth change

--- src/router/router.js
+++ src/router/router.js
@@ -7,15 +7,15 @@ const authRouter = require('./auth');
 const { passportVerificator, generateToken } = require('../middlewares/auth');
 
 
-router.get("/city/:id", passportVerificator.authenticate( "jwt", {session: false} ), getCity)
-router.get("/cities", passportVerificator.authenticate( "jwt", {session: false} ), getCities)
+router.get("/city/:id", getCity)
+router.get("/cities", getCities)
 router.post("/cities", passportVerificator.authenticate( "jwt", {session: false} ), verifyDataCity, addCity)
 router.put("/city/:id", passportVerificator.authenticate( "jwt", {session: false} ), verifyDataCity, modifyCity)
 router.delete("/cities", passportVerificator.authenticate( "jwt", {session: false} ), deleteCity)
 
-router.get("/itinerary/:id", passportVerificator.authenticate( "jwt", {session: false} ), getItinerary) //Get ONE itinerary from the DB by its ID
-router.get("/itineraries", passportVerificator.authenticate( "jwt", {session: false} ), getItineraries) //Get all the itineraries from the DB
-router.get("/itinerary", passportVerificator.authenticate( "jwt", {session: false} ), getItinerariesByCity) //Get all the itineraries of ONE city from the DB
+router.get("/itinerary/:id", getItinerary) //Get ONE itinerary from the DB by its ID
+router.get("/itineraries", getItineraries) //Get all the itineraries from the DB
+router.get("/itinerary", getItinerariesByCity) //Get all the itineraries of ONE city from the DB
 router.post("/itineraries/", passportVerificator.authenticate( "jwt", {session: false} ), verifyDataItinerary, addItinerary) //Add one itinerary to the DB
 router.put("/itinerary/:id", passportVerificator.authenticate( "jwt", {session: false} ), modifyItinerary) //Modify one itinerary, found by its ID, from the DB
 router.delete("/itineraries", passportVerificator.authenticate( "jwt", {session: false} ), deleteItinerary) //Delete ONE itinerary from the DB
