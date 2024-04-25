d2779c4bff0faa594fdbe92c3a64c7aad8c80bbd
Flor <102559480+EnrriquezF@users.noreply.github.com>
2024-04-25 20:43:49 -0300

    Update app.js
    test

--- src/app.js
+++ src/app.js
@@ -2,20 +2,11 @@ const express = require("express");
 const router = require("./router/router")
 const connectDB = require("./config/db")
 const cors = require('cors');
-const helmet = require('helmet')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      imgSrc: ["'self'"]
-      // Add other directives as needed
-    }
-  }));
-
 app.use("/api", router)
 
 app.listen(3000, () =>{
